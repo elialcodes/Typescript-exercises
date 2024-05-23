9e40cb60c03abe0b0bc97fd68205bb0b08dd31f5
Noelia <abascalcabreran@gmail.com>
2024-05-23 11:34:17 +0200

    last change

--- src/17-lodash-library.ts
+++ src/17-lodash-library.ts
@@ -1,12 +1,20 @@
-//trabajamos con la librería lodash, que no soporta Typescript
+//trabajamos con la librería no tipada lodash, que tiene muchas funciones que complementan Javascript,
+//pero que no soporta Typescript
 
 //así se implementaría la librería según viene en la documentación:
+//npm install lodash;// para instalar la librería
+
+//En el archivo cargamos la librería al completo con:
 //var _ = require('lodash');
 
-//así sería si importamos la librería, pero typescript nos da error y nos hace una sugerencia si nos posicionamos encima con el ratón,
+//Typescript nos devuelve error si escribimos sin más:
+//import _ from 'lodash';
+//pero VSC nos hace una sugerencia si nos posicionamos encima con el ratón,
 //nos indica poner esto en la terminal: npm install --save-dev @types/lodash
-//al hacerlo desaparece el error
+//así agregamos manualmente un sistema de tipos y al hacerlo desaparece el error y podemos continuar
+
 import _ from 'lodash';
+var _ = require('lodash');
 
 //creamos un array de objetos:
 const data = [
@@ -29,9 +37,9 @@ const data = [
 ];
 
 //con _. accedemos a las propiedades de la librería, VSC nos ofrece por autocompletado las que hay
-//con _.groupBy agrupará por una propiedad específica, en este caso role
+//con _.groupBy agrupamos por una propiedad específica, en este caso role
 const rta = _.groupBy(data, (item) => {
-  item.role;
+  return item.role;
 });
 console.log(rta);
 
